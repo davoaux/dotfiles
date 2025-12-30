4929ad2307fae4389aee9193885fc0bce52f616a
David Ruiz <ruiz.6789@gmail.com>
2025-12-30 20:20:01 +0100

    tidy up config

--- sway/.config/sway/status.sh
+++ sway/.config/sway/status.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+print_hdd_used() {
+  printf "hdd: %s" $(df -h --total --output=pcent | tail -n1 | xargs)
+}
+
 print_volume() {
-  printf "vol: %s" $(amixer get Master | awk -F '[][]' '/%/ { print $2; exit }')
+  vol=$(wpctl get-volume @DEFAULT_SINK@)
+  if grep -q MUTED <<< "$vol"; then
+    printf "vol: muted"
+  else
+    awk '{ printf "vol: %d%%", $2 * 100 }' <<< "$vol"
+  fi
 }
 
 print_date() {
   date +'%Y-%m-%d %X'
 }
 
-while printf "%s %s\n" "$(print_volume)" "$(print_date)"; do sleep 1; done
+while printf "%s  %s  %s\n" "$(print_hdd_used)" "$(print_volume)" "$(print_date)"; do sleep 1; done
