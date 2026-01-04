79bc83edaf8f01e3377338778e65f65eea6863ee
David Ruiz <ruiz.6789@gmail.com>
2026-01-04 21:23:55 +0100

    move zsh config to nix

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   home = {
@@ -13,10 +13,91 @@
     ];
 
     sessionVariables = {
-      # EDITOR = "emacs";
+      # should the /usr/localgo/bin entry be set here?
+      PATH = "$HOME/.local/bin:$HOME/bin:/usr/local/go/bin:$PATH";
+      EDITOR = "nvim";
+      MANPAGER = "nvim +Man!";
     };
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    defaultKeymap = "viins";
+    envExtra = ''
+      export KEYTIMEOUT=1
+    '';
+
+    history = {
+      path = "$HOME/.histfile";
+      size = 1000;
+      save = 1000;
+      ignoreAllDups = true;
+      ignoreSpace = true;
+      share = true;
+    };
+
+    autocd = false;
+
+    plugins = [
+    {
+      name = "zsh-autopair";
+      src = pkgs.fetchFromGitHub {
+        owner = "hlissner";
+        repo = "zsh-autopair";
+        rev = "master";
+        sha256 = "sha256-3zvOgIi+q7+sTXrT+r/4v98qjeiEL4Wh64rxBYnwJvQ=";
+      };
+    }
+    {
+      name = "pure-prompt-theme";
+      src = pkgs.fetchFromGitHub {
+        owner = "sindresorhus";
+        repo = "pure";
+        rev = "main";
+        sha256 = "sha256-AZSxP2g6BWoxyiSQH7yzbbbfGcwD8jgnXPPfcYwJUL0=";
+      };
+    }
+    ];
+
+    initContent = lib.mkMerge [
+      # zsh completion
+      (lib.mkOrder 550 ''
+        zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+      '')
+      # pure-prompt-theme plugin
+      (lib.mkOrder 550 ''
+        PURE_PROMPT_SYMBOL="$"
+        PURE_PROMPT_VICMD_SYMBOL="<$>"
+      '')
+      # zsh-autopair plugin
+      (lib.mkOrder 1000 ''
+        autopair-init
+      '')
+      # pure-prompt-theme plugin
+      (lib.mkOrder 1000 ''
+        autoload -U promptinit; promptinit
+        prompt pure
+      '')
+    ];
+
+    shellAliases = {
+      ls = "ls --color=auto";
+      l = "ls -lah";
+      rm = "rm -i";
+      k = "kubectl";
+      vi = "nvim";
+      fonts = "fc-list : family | sort | uniq | fzf";
+    };
+
+  };
 }
