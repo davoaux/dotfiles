2ecfd618c2e5452d21354ca2f613404776292a7c
David Ruiz <ruiz.6789@gmail.com>
2026-01-05 19:50:59 +0100

    move programs configs to modules directory

---
+++ modules/zsh.nix
@@ -0,0 +1,81 @@
+{ pkgs, lib, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+
+    defaultKeymap = "viins";
+    envExtra = ''
+      export KEYTIMEOUT=1
+    '';
+
+    history = {
+      path = "$HOME/.histfile";
+      size = 1000;
+      save = 1000;
+      ignoreAllDups = true;
+      ignoreSpace = true;
+      share = true;
+    };
+
+    autocd = false;
+
+    plugins = [
+    {
+      name = "zsh-autopair";
+      src = pkgs.fetchFromGitHub {
+        owner = "hlissner";
+        repo = "zsh-autopair";
+        rev = "master";
+        sha256 = "sha256-3zvOgIi+q7+sTXrT+r/4v98qjeiEL4Wh64rxBYnwJvQ=";
+      };
+    }
+    {
+      name = "pure-prompt-theme";
+      src = pkgs.fetchFromGitHub {
+        owner = "sindresorhus";
+        repo = "pure";
+        rev = "main";
+        sha256 = "sha256-AZSxP2g6BWoxyiSQH7yzbbbfGcwD8jgnXPPfcYwJUL0=";
+      };
+    }
+    ];
+
+    initContent =
+      let
+        zshBeforeComp = lib.mkOrder 550;
+        zshGeneral = lib.mkOrder 1000;
+
+        autopairCfg = zshGeneral "autopair-init";
+        completionCfg = zshBeforeComp ''
+          zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+        '';
+        purePromptCfg = lib.mkMerge [
+          (zshBeforeComp ''
+            PURE_PROMPT_SYMBOL="$"
+            PURE_PROMPT_VICMD_SYMBOL="<$>"
+          '')
+          (zshGeneral ''
+            autoload -U promptinit; promptinit
+            prompt pure
+          '')
+        ];
+      in
+      lib.mkMerge [
+        autopairCfg
+        completionCfg
+        purePromptCfg
+      ];
+
+    shellAliases = {
+      ls = "ls --color=auto";
+      l = "ls -lah";
+      rm = "rm -i";
+      k = "kubectl";
+      vi = "nvim";
+      fonts = "fc-list : family | sort | uniq | fzf";
+    };
+
+  };
+}
