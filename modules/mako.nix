d9d8967bc080a4aca800ec18a9b2cd14bd490e17
David Ruiz <ruiz.6789@gmail.com>
2026-01-09 10:40:19 +0100

    [dunst] replace dunst with mako

---
+++ modules/mako.nix
@@ -0,0 +1,12 @@
+{ ... }:
+
+{
+  programs.mako = {
+    enable = true;
+    settings = {
+      font = "SFUIDisplay-Medium 12";
+      background-color = "#000000";
+      margin = 20;
+    };
+  };
+}
