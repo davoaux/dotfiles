3079c06dccbf6af0cc2da8a4192ce3a6718a6f47
David Ruiz <ruiz.6789@gmail.com>
2026-01-19 19:54:43 +0100

    [*] remove usb-notifier manual clone

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,11 +9,4 @@ fi
 [[ ! -d ~/.dotfiles ]] && git clone https://github.com/davoaux/dotfiles.git ~/.dotfiles
 [[ ! -d ~/bin ]] && git clone https://github.com/davoaux/bin.git ~/bin
 
-# install usb-notifier
-if [[ "$OSTYPE" = "linux"* ]]; then
-  if [[ -f ~/bin/usb-notifier/install.sh && -x ~/bin/usb-notifier/install.sh ]]; then
-    ! sudo ~/bin/usb-notifier/install.sh && echo "usb-notifier installation failed - check output for missing dependencies"
-  fi
-fi
-
 echo "Bootstrap complete. Run 'nix run home-manager -- switch --flake ~/.dotfiles#$(hostname)' to finish setup"
