422036b0d59d87df157fc2b7a4ebd12413773700
David Ruiz <david.ruiz@glovoapp.com>
2026-01-27 16:39:34 +0100

    [ideavim] persist config and fix go to error mapping

---
+++ modules/darwin/ideavim.nix
@@ -0,0 +1,8 @@
+{ config, ... }:
+
+{
+  # TODO only if intellij is installed
+  home.file = {
+    ".ideavimrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/ideavimrc/.ideavimrc";
+  };
+}
